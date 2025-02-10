42406147542bb6d399a715291e6d9c643ade8a63
teta0412 <ducna0412@gmail.com>
2025-02-10 18:36:40 +0700

    Added application for each service

---
+++ notification-service/src/main/java/org/teta/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.teta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class NotificationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+}
---
+++ websocket-service/src/main/java/org/teta/WebSocketServiceApplication.java
@@ -0,0 +1,14 @@
+package org.teta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class WebSocketServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketServiceApplication.class, args);
+    }
+}
